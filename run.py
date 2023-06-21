6b7eca78375d9f7013d6284ddecff9ff5ff2eb31
Ross Boczar <rjboczar@gmail.com>
2023-06-21 13:14:35 -0700

    cleaning up parameterizations

--- run.py
+++ run.py
@@ -8,13 +8,37 @@ from fair_participation.main import localized_rho_fn, run_problem, compare, comp
 log = logging.getLogger(__name__)
 
 
-def main():
+def main(problems: dict[str, dict]):
     # TODO clean dir version
     # Create needed directories if they don't exist
     for folder in ("losses", "data", "mp4", "npy", "pdf"):
         os.makedirs(folder, exist_ok=True)
 
-    problems = {
+    for problem, kwargs in problems.items():
+        run_problem(problem, method="RRM", save_init=False, **kwargs)
+        # run_problem(problem, method="RRM_grad", save_init=False, jit=True, **kwargs)
+        run_problem(problem, method="LPU", save_init=False, **kwargs)
+        # # run_problem(problem, method="LPU_grad", save_init=False, jit=True, **kwargs)
+        run_problem(problem, method="Fair", save_init=False, **kwargs)
+        # # run_problem(problem, method="Fair_grad", save_init=False, jit=True, **kwargs)
+        run_problem(
+            problem,
+            method=None,
+            save_init=True,
+            t_rrm=np.load(f"npy/{problem}_RRM_thetas.npy")[-1],
+            t_lpu=np.load(f"npy/{problem}_LPU_thetas.npy")[-1],
+            t_fair=np.load(f"npy/{problem}_Fair_thetas.npy")[-1],
+            t_init=kwargs["init_theta"],
+            **kwargs,
+        )
+        compare(problem, grad=False)
+        # compare(problem, grad=True)
+
+    # compare_2("Income")
+
+
+if __name__ == "__main__":
+    probs = {
         "Income": {
             "rho_fns": (localized_rho_fn(-0.75, 20),),
             "init_theta": 0.57 * np.pi / 2,
@@ -36,29 +60,4 @@ def main():
             "theta_plot_range": [0.4 * np.pi / 2, 0.6 * np.pi / 2],
         },
     }
-
-    for problem, kw in problems.items():
-        run_problem(problem, method="RRM", save_init=False, **kw)
-        # # run_problem(problem, method="RRM_grad", save_init=False, jit=True, **kw)
-        run_problem(problem, method="LPU", save_init=False, **kw)
-        # # run_problem(problem, method="LPU_grad", save_init=False, jit=True, **kw)
-        run_problem(problem, method="Fair", save_init=False, **kw)
-        # # run_problem(problem, method="Fair_grad", save_init=False, jit=True, **kw)
-        run_problem(
-            problem,
-            method=None,
-            save_init=True,
-            t_rrm=np.load(f"npy/{problem}_RRM_thetas.npy")[-1],
-            t_lpu=np.load(f"npy/{problem}_LPU_thetas.npy")[-1],
-            t_fair=np.load(f"npy/{problem}_Fair_thetas.npy")[-1],
-            t_init=kw["init_theta"],
-            **kw,
-        )
-        compare(problem, grad=False)
-        # compare(problem, grad=True)
-
-    # compare_2("Income")
-
-
-if __name__ == "__main__":
-    main()
+    main(probs)
