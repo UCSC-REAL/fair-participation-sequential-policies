c09c178ce558aa2df76c53463bc2e476e385b27a
Ross Boczar <rjboczar@gmail.com>
2023-08-18 10:44:28 -0700

    cleaning up environment code

--- src/fair_participation/environment.py
+++ src/fair_participation/environment.py
@@ -1,24 +1,94 @@
 import os
-from typing import Callable, Optional
+from typing import Callable
 
 import jax.numpy as jnp
 from jax import Array
 from jax.typing import ArrayLike
 from scipy.spatial import ConvexHull
 
-
-from fair_participation.rate_functions import concave_rho_fn
-from fair_participation.folktasks import achievable_loss as get_achievable_loss
-
-from fair_participation.loss_functions import values_and_grads_fns
-from fair_participation.state import StateInfo
-
 from fair_participation.base_logger import logger
+from fair_participation.folktasks import achievable_loss as get_achievable_loss
+from fair_participation.loss_functions import values_and_grads_fns
+from fair_participation.rate_functions import concave_rho_fn
+from fair_participation.state import StateInfo
 from fair_participation.utils import PROJECT_ROOT
 
 
-def get_env_filename(name):
+def get_env_filename(name: str):
     return os.path.join(PROJECT_ROOT, "losses", f"{name}.npy")
+
+
+class Environment:
+    def __init__(
+        self,
+        name: str,
+        achievable_loss: ArrayLike,
+        rho_fns: tuple[Callable[[ArrayLike], Array]],
+        group_sizes: ArrayLike,
+        init_loss_direction: ArrayLike,
+        fair_epsilon: float = 0.01,
+    ) -> None:
+        """
+        Environment for running the simulation.
+
+        :param name: Name of the problem.
+        :param achievable_loss: Array (n x # of groups) of achievable losses.
+        :param rho_fns: Tuple of functions (one per group) that map loss -> participation rate.
+        :param group_sizes: Array of group sizes summing to 1.
+        :param init_loss_direction: Initial direction of loss. Will choose achievable loss closest to this direction.
+        :param fair_epsilon: Fairness parameter.
+        :return: Environment object.
+        """
+        self.name = name
+        self.group_sizes = group_sizes
+        self._init_loss_direction = init_loss_direction
+        self.fair_epsilon = fair_epsilon
+
+        self.achievable_loss = achievable_loss
+
+        # filter and reorder points
+        self.achievable_loss = self.achievable_loss[
+            ConvexHull(self.achievable_loss).vertices
+        ]
+
+        self.loss_hull = ConvexHull(self.achievable_loss)
+
+        # values_and_grads:
+        # loss (vector) -> {rho (vector),
+        #                   total_loss (scalar),
+        #                   grad_total_loss (vector),
+        #                   full_deriv_total_loss (vector),
+        #                   disparity (scalar),
+        #                   grad_disparity_loss (vector)}   [dH/dl]
+        self.values_and_grads = values_and_grads_fns(
+            rho_fns,
+            self.group_sizes,
+            self.fair_epsilon,
+        )
+
+        # Take initial loss to be the achievable loss closest to the initial angle
+        unit_init_loss = self._init_loss_direction / jnp.linalg.norm(
+            self._init_loss_direction
+        )
+        closest_idx = jnp.argmax(
+            self.achievable_loss
+            @ unit_init_loss
+            / jnp.linalg.norm(self.achievable_loss, axis=1)
+        )
+        self.init_loss = self.achievable_loss[closest_idx]
+        vgs = self.values_and_grads(self.init_loss)
+
+        # Conceptually dirty, since there's state information
+        # that updates "out-of-phase" and belongs to
+        # optmization method
+        self.state: StateInfo = StateInfo(
+            loss=self.init_loss,
+            rho=vgs["rho"],
+            total_loss=vgs["total_loss"],
+            disparity=vgs["disparity"],
+            linear_weights=vgs["rho"],
+            lambda_estimate=0,
+        )
 
 
 def make_environment(
@@ -27,16 +97,17 @@ def make_environment(
     init_loss_direction: float | ArrayLike = 0.6,
     fair_epsilon: float = 0.01,
     n_loss_samples: int = 100,
-    **_,
-) -> None:
+) -> Environment:
     """
-    prepare environment for a given problem.
-    Does caching
+    Prepares environment for a given problem. Will cache values if possible.
 
     :param name: Name of the problem
     :param rho_fns: Rho functions for each group, or a single rho function for all groups. Defaults to concave_rho_fn.
     :param init_loss_direction: Initial direction of loss. Will choose achievable loss closest to this direction.
      Can be set to float to match legacy init_theta.
+    :param fair_epsilon: Fairness parameter.
+    :param n_loss_samples: Number of samples to use for caching achievable loss.
+    :return: Environment object for the problem.
     """
 
     logger.info(f"Setting up env for problem {name}.")
@@ -83,72 +154,3 @@ def make_environment(
         init_loss_direction=init_loss_direction,
         fair_epsilon=fair_epsilon,
     )
-
-
-class Environment:
-    def __init__(
-        self,
-        name,
-        achievable_loss: ArrayLike,
-        rho_fns: tuple[Callable[[ArrayLike], Array]],
-        group_sizes: ArrayLike,
-        init_loss_direction: ArrayLike,
-        fair_epsilon: float = 0.01,
-    ) -> None:
-        """
-        Environment for running the simulation.
-
-        :param achievable_loss: Array (n x # of groups) of achievable losses.
-        :param rho_fns: Tuple of functions (one per group) that map loss -> participation rate.
-        :param group_sizes: Array of group sizes summing to 1.
-        :param init_loss_direction: Initial direction of loss. Will choose achievable loss closest to this direction.
-        """
-        self.name = name
-        self.group_sizes = group_sizes
-        self._init_loss_direction = init_loss_direction
-        self.fair_epsilon = fair_epsilon
-
-        self.achievable_loss = achievable_loss
-
-        # filter and reorder points
-        self.achievable_loss = self.achievable_loss[
-            ConvexHull(self.achievable_loss).vertices
-        ]
-
-        self.loss_hull = ConvexHull(self.achievable_loss)
-
-        # values_and_grads:
-        # loss (vector) -> {rho (vector),
-        #                   total_loss (scalar),
-        #                   grad_total_loss (vector),
-        #                   disparity (scalar),
-        #                   grad_disparity_loss (vector)}   [dH/dl]
-        self.values_and_grads = values_and_grads_fns(
-            rho_fns,
-            self.group_sizes,
-            self.fair_epsilon,
-        )
-
-        # Take initial loss to be the achievable loss closest to the initial angle
-        unit_init_loss = self._init_loss_direction / jnp.linalg.norm(
-            self._init_loss_direction
-        )
-        closest_idx = jnp.argmax(
-            self.achievable_loss
-            @ unit_init_loss
-            / jnp.linalg.norm(self.achievable_loss, axis=1)
-        )
-        self.init_loss = self.achievable_loss[closest_idx]
-        vgs = self.values_and_grads(self.init_loss)
-
-        # Conceptually dirty, since there's state information
-        # that updates "out-of-phase" and belongs to
-        # optmization method
-        self.state: StateInfo = StateInfo(
-            loss=self.init_loss,
-            rho=vgs["rho"],
-            total_loss=vgs["total_loss"],
-            disparity=vgs["disparity"],
-            linear_weights=vgs["rho"],
-            lambda_estimate=0,
-        )
